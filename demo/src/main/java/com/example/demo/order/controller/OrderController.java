42d924108c44c8154bca3e06cab1be14c40fa186
dnjawm19 <dnjawm19@naver.com>
2024-08-23 21:28:58 +0900

    refactor: url order->orders

--- demo/src/main/java/com/example/demo/order/controller/OrderController.java
+++ demo/src/main/java/com/example/demo/order/controller/OrderController.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/order")
+@RequestMapping("/orders")
 public class OrderController {
 
     private final OrderService orderService;
