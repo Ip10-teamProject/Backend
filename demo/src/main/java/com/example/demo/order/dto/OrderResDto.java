4f15d5ff2be1b52c5b0fca6b87d92d37a126c210
dnjawm19 <dnjawm19@naver.com>
2024-08-23 22:02:08 +0900

    refactor: orderResDto에 order status 추가

--- demo/src/main/java/com/example/demo/order/dto/OrderResDto.java
+++ demo/src/main/java/com/example/demo/order/dto/OrderResDto.java
@@ -1,6 +1,7 @@
 package com.example.demo.order.dto;
 
 import com.example.demo.order.entity.Order;
+import com.example.demo.order.entity.OrderStatus;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,12 +15,14 @@ public class OrderResDto {
     private UUID paymentId;
     private UUID storeId;
     private String memo;
+    private OrderStatus status;
 
-    public OrderResDto(Order order){
+    public OrderResDto(Order order) {
         this.id = order.getId();
         this.userId = order.getUserId();
         this.paymentId = order.getPaymentId();
         this.storeId = order.getStoreId();
         this.memo = order.getMemo();
+        this.status = order.getStatus();
     }
 }
