33861cd8274e3bc90403c7015353ea5622090f39
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-08-26 17:35:27 +0900

    finish: jwt auth

--- demo/src/main/java/com/example/demo/users/controller/UserController.java
+++ demo/src/main/java/com/example/demo/users/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.example.demo.users.controller;
 
-import com.example.demo.users.application.UserInfoDto;
 import com.example.demo.users.application.UserService;
+import com.example.demo.users.application.dto.UserInfoDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
