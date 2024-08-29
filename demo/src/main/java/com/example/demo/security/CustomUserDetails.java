a87529d430fe71d4e7f33773309577c0a73ae1f4
JeongSu <warms1995@gmail.com>
2024-08-29 11:06:56 +0900

    refactor: 인증객체에 유저객체 포함시키도록 수정 (#18)

--- demo/src/main/java/com/example/demo/security/CustomUserDetails.java
+++ demo/src/main/java/com/example/demo/security/CustomUserDetails.java
@@ -1,45 +1,33 @@
 package com.example.demo.security;
 
-import com.example.demo.users.domain.UserRoleEnum;
+import com.example.demo.users.domain.User;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.Collections;
 
-@Getter
 @Setter
 public class CustomUserDetails implements UserDetails {
 
+    // Getters
+    @Getter
     private Long id;
-    private String username;
-    private String password;
+
     private Collection<? extends GrantedAuthority> authorities;
 
-    // Constructor
-    public CustomUserDetails(Long id, String username, String password, UserRoleEnum role) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.authorities = Collections.singletonList(new SimpleGrantedAuthority(role.getAuthority()));
-    }
-
-    // Getters
-    public Long getId() {
-        return id;
-    }
+    @Getter
+    private User user;
 
     @Override
     public String getUsername() {
-        return username;
+        return user.getUsername();
     }
 
     @Override
     public String getPassword() {
-        return password;
+        return null;
     }
 
     @Override
--- demo/src/main/java/com/example/demo/security/CustomUserDetailsService.java
+++ demo/src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -2,9 +2,14 @@ package com.example.demo.security;
 
 import com.example.demo.users.domain.User;
 import com.example.demo.users.domain.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -18,13 +23,16 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public CustomUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
-        return new CustomUserDetails(
-                user.getId(),
-                user.getUsername(),
-                user.getPassword(),
-                user.getRole()
-        );
+        CustomUserDetails userDetails = new CustomUserDetails();
+
+        Collection<? extends GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority(user.getRole().getAuthority()));
+
+        userDetails.setUser(user);
+        userDetails.setId(user.getId());
+        userDetails.setAuthorities(authorities);
+
+        return userDetails;
     }
 }
