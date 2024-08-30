23b8a60a9d9bd5f3a7dc7f71374cd579ae9eaf1a
Leejacksoon <gyeongsik1012@gmail.com>
2024-08-30 10:28:11 +0900

    category , location product 권한

--- demo/src/main/java/com/example/demo/location/controller/LocationController.java
+++ demo/src/main/java/com/example/demo/location/controller/LocationController.java
@@ -6,6 +6,7 @@ import com.example.demo.location.dto.UpdateLocationRequestDto;
 import com.example.demo.location.service.LocationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,22 +18,27 @@ import java.util.UUID;
 public class LocationController {
     private final LocationService locationService;
 
+
+    @PreAuthorize("hasAnyRole('MASTER')")
     @PostMapping("")
     public ResponseEntity<List<LocationResponseDto>> addLocations(@RequestBody LocationRequestDto locationRequestDto) {
         return ResponseEntity.ok()
                 .body(locationService.addLocations(locationRequestDto));
     }
+    @PreAuthorize("permitAll()")
     @GetMapping("")
     public ResponseEntity<List<LocationResponseDto>> getLocations() {
         return ResponseEntity.ok()
                 .body(locationService.getLocations());
     }
+    @PreAuthorize("hasAnyRole('MASTER')")
     @DeleteMapping("/{locationId}")
     public ResponseEntity<String> deleteLocation(@PathVariable UUID locationId) {
         locationService.deleteLocation(locationId);
         return ResponseEntity.ok()
                 .body(locationId+"위치"+" "+"삭제완료");
     }
+    @PreAuthorize("hasAnyRole('MASTER')")
     @PutMapping("/{locationId}")
     public ResponseEntity<LocationResponseDto> updateLocation(@PathVariable UUID locationId,@RequestBody UpdateLocationRequestDto updateLocationRequestDto) {
         return ResponseEntity.ok()
