0d0d72703231a16bcf4008734eaed02d6f9aef5e
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-08-30 10:49:42 +0900

    feat: menu-delete request dto

---
+++ demo/src/main/java/com/example/demo/menu/dto/StoreMenusDeleteRequestDto.java
@@ -0,0 +1,16 @@
+package com.example.demo.menu.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StoreMenusDeleteRequestDto {
+    private List<UUID> menuIds = new ArrayList<>();
+}
